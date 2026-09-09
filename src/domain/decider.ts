c8afcb4345ff69f352d5b536a96bcb3bf1d818a0
Karsten Pruehss <karsten.pruehss@tabcorp.com.au>
2026-09-09 14:58:11 +1000

    Add Decider interface

---
+++ src/domain/decider.ts
@@ -0,0 +1,8 @@
+import { Effect } from "effect";
+
+export interface Decider<Command, State, Event, DomainError> {
+  readonly decide: (command: Command, state: State) => Effect.Effect<ReadonlyArray<Event>, DomainError>;
+  readonly evolve: (state: State, event: Event) => State;
+  readonly initialState: State;
+  readonly isTerminal: (state: State) => boolean;
+}
